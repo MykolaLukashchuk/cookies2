720ef21544a6753f45b88855aee1ad7c3e7c6215
Nick Lukashchuk <nm.luka@gmail.com>
2019-03-03 09:47:32 +0200

    init

---
+++ src/main/java/server/Server.java
@@ -0,0 +1,110 @@
+package server;
+
+import akka.actor.ActorSystem;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.model.Uri;
+import akka.http.javadsl.model.headers.Location;
+import akka.http.javadsl.server.HttpApp;
+import akka.http.javadsl.server.Route;
+import akka.http.javadsl.server.values.PathMatcher;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import javax.inject.Inject;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
+
+import static akka.http.javadsl.marshallers.jackson.Jackson.jsonAs;
+import static akka.http.javadsl.model.HttpResponse.create;
+import static akka.http.javadsl.server.RequestVals.entityAs;
+import static akka.http.javadsl.server.values.PathMatchers.uuid;
+import static akka.http.scaladsl.model.StatusCodes.*;
+
+public class Server extends HttpApp {
+
+
+    private final GroupRepo groups;
+    private final UsersService usersService;
+
+    @Inject
+    public Server(GroupRepo groups, UsersService usersService) {
+        this.groups = groups;
+        this.usersService = usersService;
+    }
+
+    public static void main(String[] args) throws IOException {
+        ActorSystem akkaSystem = ActorSystem.create("akka-http-example");
+        Injector injector = Guice.createInjector(new AppModule());
+        injector.getInstance(Server.class).bindRoute("0.0.0.0", 8080, akkaSystem);
+
+        System.out.println("<ENTER> to exit!");
+        System.in.read();
+        akkaSystem.shutdown();
+    }
+
+    @Override
+    public Route createRoute() {
+
+        /* Dezelfde instantie moet gebruikt worden voor het opvragen van een PathMatcher! */
+        PathMatcher<UUID> uuidExtractor = uuid();
+
+        return handleExceptions(e -> {
+                    e.printStackTrace();
+                    return complete(create().withStatus(InternalServerError()));
+                },
+                pathSingleSlash().route(
+                        getFromResource("web\\index.html")
+                ),
+                pathPrefix("groups").route(
+                        get(pathEndOrSingleSlash().route(
+                                handleWith(ctx -> ctx.completeAs(Jackson.json(), groups.getAll()))
+                        )),
+                        get(path(uuidExtractor).route(
+                                handleWith(uuidExtractor,
+                                        (ctx, uuid) -> ctx.completeAs(Jackson.json(), groups.get(uuid))
+                                )
+                        )),
+                        post(
+                                handleWith(entityAs(jsonAs(Group.class)),
+                                        (ctx, group) -> {
+                                            Group saved = groups.create(group);
+                                            return
+                                                    ctx.complete(HttpResponse.create()
+                                                            .withStatus(Created())
+                                                            .addHeader(
+                                                                    Location.create(
+                                                                            Uri.create("http://localhost:8080/groups/" + saved.getUuid()))));
+                                        }
+                                )
+                        ),
+                        put(path(uuidExtractor).route(
+                                handleWith(uuidExtractor, entityAs(jsonAs(Group.class)),
+                                        (ctx, uuid, group) -> {
+                                            if (!Objects.equals(group.getUuid(), uuid))
+                                                return ctx.completeWithStatus(BadRequest());
+                                            else {
+                                                groups.update(group);
+                                                return ctx.completeWithStatus(OK());
+                                            }
+                                        }
+                                )
+                        )),
+                        put(path(uuidExtractor).route(
+                                handleWith(uuidExtractor,
+                                        (ctx, uuid) -> {
+                                            groups.delete(uuid);
+                                            return ctx.completeWithStatus(OK());
+                                        }
+                                )
+                        ))
+                ),
+                pathPrefix("users").route(
+                        get(pathEndOrSingleSlash().route(
+                                handleWith(requestContext -> requestContext.completeAs(Jackson.json(), usersService.sayHi()))
+                        ))
+                )
+        );
+    }
+}
---
+++ src/main/java/server/UsersService.java
@@ -0,0 +1,8 @@
+package server;
+
+public class UsersService {
+
+    public String sayHi() {
+        return "Hi!";
+    }
+}
